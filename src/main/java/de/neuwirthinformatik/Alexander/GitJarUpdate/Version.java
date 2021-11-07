18d9a786216f0fbd7d644d2e3bf9b5c78664fc27
APN-Pucky <APN-Pucky@users.noreply.github.com>
2021-11-07 11:16:38 +0100

    More general versions

--- src/main/java/de/neuwirthinformatik/Alexander/GitJarUpdate/Version.java
+++ src/main/java/de/neuwirthinformatik/Alexander/GitJarUpdate/Version.java
@@ -15,7 +15,9 @@ public class Version implements Comparable<Version> {
         if(version == null)
             throw new IllegalArgumentException("Version can not be null");
         if(!version.matches("[0-9]+(\\.[0-9]+)*"))
-            throw new IllegalArgumentException("Invalid version format");
+            throw new IllegalArgumentException("Invalid version format: " + version);
+        if(!version.matches("v[0-9]+(\\.[0-9]+)*"))
+        	version = version.substring(1);
         this.version = version;
     }
 
